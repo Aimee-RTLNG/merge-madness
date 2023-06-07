48ea4597fbc50af311aaac6e022c61728c4bbe4b
Ullus <nikita96piter@gmail.com>
2023-06-07 20:30:13 +0300

    first version of discover page

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,139 @@
-<template>11</template>
+<template>
+	<div class="page">
+		<header class="nav">
+			<ul class="nav__items">
+				<li class="nav__item nav__item--logo">Cook</li>
+				<li class="nav__item nav__item--hamburger">=</li>
+			</ul>
+			<h4 class="nav__directory">Discover</h4>
+		</header>
+		<section class="explore">
+			<div class="slider__container">
+				<!-- card start -->
+				<div class="card">
+					<div class="card__img-container">
+						<img src="@/assets/card-1.jpg" alt="#" class="card__img" />
+					</div>
+					<p class="card__title">
+						Fishy-Fishy, roasted fish with vegetables and lemon and lemon and
+						lemon and lemon and lemon and lemon
+					</p>
+				</div>
+				<!-- card end -->
+				<!-- card start -->
+				<div class="card">
+					<div class="card__img-container">
+						<img src="@/assets/card-2.jpg" alt="#" class="card__img" />
+					</div>
+					<p class="card__title">Asian ramen with egg and pork</p>
+				</div>
+				<!-- card end -->
+			</div>
+		</section>
+		<section class="browse">
+			<div>
+				<p class="browse__header">Browse filters ▼</p>
+			</div>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique,
+				adipisci tempore, facilis maxime voluptatem voluptas soluta iste
+				laboriosam aliquam dolor maiores veritatis doloremque, consequatur
+				molestiae a magni voluptates repudiandae earum aperiam. Consectetur
+				assumenda facilis adipisci autem maxime quisquam eligendi praesentium
+				dolor veritatis! Quasi ducimus laboriosam ab unde reiciendis. Esse nobis
+				ex quisquam assumenda eos necessitatibus soluta consequatur similique
+				eaque, adipisci et omnis enim error. Ut animi adipisci sapiente! Qui
+				quam, ducimus nihil nam mollitia dolorum officiis quisquam iure culpa
+				maiores reiciendis assumenda eos sed perspiciatis, atque consequuntur
+				delectus cum! Ad rerum cum optio officia sit nam similique, nisi cumque
+				maxime perspiciatis praesentium libero architecto voluptas doloremque
+				sed corporis! Aut, sed libero beatae itaque tempora quas ratione quo?
+				Quidem saepe aliquam tempora quo excepturi! Reiciendis, nemo. Voluptates
+				vero hic rem ea, repellat modi. Dolorem debitis modi excepturi
+				perferendis repellat beatae accusantium est possimus, illo hic ratione
+				quod aperiam, eum neque cumque necessitatibus aut animi vitae quibusdam.
+				Harum, quia qui! Excepturi fugiat, minima impedit eius voluptates aut
+				eaque iste alias maxime, minus itaque, unde non harum. Ad tempora cumque
+				velit fugit praesentium veritatis deserunt molestiae perferendis nostrum
+				dignissimos unde aliquid neque cum sit, incidunt quidem sed quae,
+				inventore iusto. Inventore nesciunt quaerat, illo repellendus quod quasi
+				ipsum unde libero tempore adipisci qui consequatur nobis consequuntur
+				corporis quis in cupiditate animi quia eaque labore nihil, temporibus
+				eius. Veniam quisquam deserunt, beatae ad nihil magni doloremque et
+				aperiam fugiat odio aliquam consectetur alias repudiandae, voluptatum
+				nesciunt repellendus. Quasi itaque molestias aliquid harum repudiandae
+				laboriosam nemo culpa rem tempora? Voluptatem dignissimos sequi quod
+				sed, et provident facilis aliquam pariatur in neque voluptatum quos
+				minus repudiandae voluptate assumenda explicabo quibusdam aut autem
+				similique dicta architecto temporibus accusamus. Debitis architecto
+				reiciendis nostrum deserunt quod iste, est inventore dicta, ab earum
+				fuga quam tempora aspernatur voluptatum sapiente repudiandae consectetur
+				beatae saepe. Quo quod, perferendis sunt eveniet maiores optio, porro
+				deserunt dolor minima provident neque aut debitis nulla pariatur
+				assumenda nihil voluptatem obcaecati. Deleniti, facilis expedita? Fuga
+				eius beatae, eum nesciunt reiciendis quae accusamus, nam consequuntur
+				impedit velit quis, iure qui. Non inventore eum cumque magni. Unde eos
+				nemo illo facere exercitationem, ad laborum aperiam distinctio quod
+				natus vero voluptates repellendus quo reprehenderit soluta nam assumenda
+				sunt? Debitis quisquam quae unde odit tenetur quos totam qui expedita
+				blanditiis veritatis reprehenderit quasi neque saepe minus, maxime
+				incidunt voluptatem dignissimos non cum, deleniti voluptatum itaque,
+				eveniet sint? Nam, voluptatem earum. Placeat molestias recusandae quas
+				earum iure blanditiis aliquam consequuntur ipsam voluptatum itaque
+				tempore quam repudiandae, soluta vel suscipit! Minus ipsa neque quaerat
+				dicta accusamus, modi quibusdam magni mollitia debitis fuga voluptatum
+				deserunt esse velit animi. Inventore repudiandae ratione vitae molestiae
+				sit quo, temporibus quasi corporis aut, molestias, ut maxime sequi
+				asperiores! Eum, minima reprehenderit sapiente voluptatum, quis eius
+				deserunt quae repellat doloremque voluptatibus laboriosam natus fuga
+				odio libero id saepe eveniet nihil ipsum veniam! Repellendus est, iusto
+				minus molestiae maxime repellat deleniti doloremque odio nulla
+				reiciendis quidem cumque esse rerum ad, voluptatem, necessitatibus
+				laborum beatae iure error tempora? Molestiae amet quis, a eligendi fugit
+				dignissimos architecto magni earum sint quaerat accusamus officiis et
+				quae ex ipsa aut repellendus placeat cupiditate provident atque ab
+				porro! Vero illum at ea, iusto perferendis voluptates repellendus
+				aliquam omnis quibusdam commodi quaerat debitis eaque nemo. Repellendus
+				officia pariatur necessitatibus alias consequatur, minima quidem eveniet
+				nisi praesentium ad in tempore reprehenderit dolores.
+			</p>
+		</section>
+		<div class="footer__space"></div>
+		<footer class="footer">
+			<ul class="footer__items">
+				<li class="footer__item active">
+					<div class="footer__btn">
+						<div class="footer__icon">
+							<i class="fa-regular fa-compass"></i>
+						</div>
+						<p class="footer__desc">Discover</p>
+					</div>
+				</li>
+				<li class="footer__item">
+					<div class="footer__btn">
+						<div class="footer__icon">
+							<i class="fa-solid fa-book"></i>
+						</div>
+						<p class="footer__desc">Recipes</p>
+					</div>
+				</li>
+				<li class="footer__item">
+					<div class="footer__btn">
+						<div class="footer__icon">
+							<i class="fa-regular fa-bookmark"></i>
+						</div>
+						<p class="footer__desc">Favourite</p>
+					</div>
+				</li>
+				<li class="footer__item">
+					<div class="footer__btn">
+						<div class="footer__icon"><i class="fa-regular fa-user"></i></div>
+						<p class="footer__desc">Profile</p>
+					</div>
+				</li>
+			</ul>
+		</footer>
+	</div>
+</template>
 
-<style lang="scss">
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-}
-
-nav {
-	padding: 30px;
-
-	a {
-		font-weight: bold;
-		color: #2c3e50;
-
-		&.router-link-exact-active {
-			color: #42b983;
-		}
-	}
-}
-</style>
+<style lang="scss"></style>
